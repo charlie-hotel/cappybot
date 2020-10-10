ac248a86473f1517acddac0846d3141a5c756e9d
doop <doop@shopkosovo.com>
2020-10-10 10:41:27 -0700

    add function to query SharktasticA's keyboard database

--- clackbot.py
+++ clackbot.py
@@ -1,10 +1,14 @@
-import os
 import discord
 import emoji
+import os
 import random
+import requests
 
 from discord.ext import commands
 from dotenv import load_dotenv
+
+# Set version number
+VERSION_NUMBER = 0.3
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +21,16 @@ bot = commands.Bot(command_prefix='!')
 
 
 # Set up bot commands
+@bot.command(name='version')
+async def say_version(context):
+    """Display clackbot version information."""
+    await context.send(f'clackbot {VERSION_NUMBER} © 2020 <@!572963354902134787>')
+
+@bot.command(name='source')
+async def say_source_url(context):
+    """Display the link to clackbot's source code on GitHub."""
+    await context.send('https://github.com/doopqoob/clackbot')
+
 @bot.command(name='follow')
 async def join_voice(context):
     """Follow a user into a voice channel."""
@@ -43,13 +57,17 @@ async def play_clacking(context):
     # randomize list
     random.shuffle(filenames)
 
-    # set up audio source
+    # set up voice channel
     guild = context.guild
     voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
-    audio_source = discord.FFmpegPCMAudio(f'clacks/{filenames[0]}')
 
     if voice_client is None:
+        await context.send("I need to be in a voice channel to do that. Join the desired voice channel "
+                           + "then type !follow in this channel.")
         return
+
+    # set up audio source
+    audio_source = discord.FFmpegPCMAudio(f'clacks/{filenames[0]}')
 
     if not voice_client.is_playing():
         voice_client.play(audio_source, after=None)
@@ -111,6 +129,74 @@ async def poll(context, *args):
         emoji_unicode = emoji.emojize(emoji_desc, use_aliases=True)
 
         await message.add_reaction(emoji_unicode)
+
+
+@bot.command(name='kbdb')
+async def query_kb_db(context, part_num=None):
+    """Query SharktasticA's model M keyboard database by part number"""
+    # Make sure the user has entered a part number
+    if part_num is None:
+        await context.send("ERROR: No keyboard part number provided.")
+        return
+
+    # These are all the fields we're going to request from the database
+    # (and their long names for when we display the data in chat)
+    fields_dict = {'pn':            "Part Number",
+                   'fru':           "FRU Part Number",
+                   'name':          "Full Name",
+                   'type':          "Type",
+                   'shorthand':     "Shorthand",
+                   'nickname':      "Nickname",
+                   'model':         "Marketing Name",
+                   'oem':           "OEM (Manufacturer)",
+                   'switches':      "Switches",
+                   'date':          "First Appeared",
+                   'keys':          "Key Count",
+                   'keycaps':       "Keycap Type",
+                   'case':          "Case Colour",
+                   'branding':      "Branding",
+                   'feet':          "Feet Type",
+                   'protocol':      "Protocol",
+                   'connection':    "Connection",
+                   'layout':        "Layout/Language",
+                   'mouse':         "Int. Pointing Device",
+                   'notes':         "Notes"}
+
+    # Extract just the field names from fields_dict
+    requested_fields = fields_dict.keys()
+
+    # Turn the list of fields into a comma-delimited string:
+    fields = ','.join(requested_fields)
+
+    # Build the url
+    url = f'https://sharktastica.co.uk/kb_db_req.php?pn={part_num}&dat=JSON&fields={fields}'
+
+    # Query the DB
+    result = requests.get(url)
+
+    # Convert JSON into a python data structure
+    result = result.json()
+
+    # Extract only the first element of the result
+    result = result[0]
+
+    if result['success'] is False:
+        await context.send(f"ERROR: Part number {part_num} not found in database.")
+        return
+
+    # Take out 'success' -- there's no corresponding key in the fields_dict and no need for it anymore
+    del result['success']
+
+    # ---> Build the response <---
+    response = f"Here's what I found about part {part_num}:\n"
+    for key in result.keys():
+        if result[key] is not None:
+            response += f'**{fields_dict[key]}:** {result[key]}\n'
+    response += ('-' * 10) + '\n'
+    response += 'Learn about where this data came from: https://sharktastica.co.uk/about.php#Sources'
+
+    # aaand send it off!
+    await context.send(response)
 
 
 # run the bot
