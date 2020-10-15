1d764d3ae668b55328060156134e203463176b4d
doop <doop@shopkosovo.com>
2020-10-15 09:00:09 -0700

    display quote vote score

--- clackbot.py
+++ clackbot.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from uuid import UUID
 
 # Set version number
-VERSION_NUMBER = 0.4
+VERSION_NUMBER = "0.4.1 beta"
 
 # Load environment variables from .env file
 load_dotenv()
